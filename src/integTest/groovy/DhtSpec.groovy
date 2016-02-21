99b0c33f6735c487093c8c1903bd50ce2f24c59e
Miles Porter <milesporter@Miles-2015-Mac-Book-Pro.local>
2016-02-21 09:13:05 -0600

    Adding basic spock tests for endpoints.

--- src/integTest/groovy/DhtSpec.groovy
+++ src/integTest/groovy/DhtSpec.groovy
@@ -9,16 +9,40 @@ import spock.lang.Specification
 
 @ContextConfiguration(loader = SpringApplicationContextLoader, classes = DhtMonitorApplication)
 @WebAppConfiguration
-@IntegrationTest("server.port:8888")
-class PrimeSpec extends Specification {
+@IntegrationTest
+class DhtSpec extends Specification {
 
     @Value('${local.server.port}')
     int port;
 
     def "It should have a valid endpoint"() {
         expect:
-        def client = new RESTClient("http://localhost:8888/dhtmonitor/")
-        def resp = client.get(path : "dht")
+        def client = new RESTClient("http://localhost:8888/dhtmonitor")
+        def resp = client.get(path : "/dhtmonitor/hello")
+        assert(resp.status==200)
+    }
+
+    // you should be able to add a point
+    def "you should be able to add a point"() {
+        expect:
+        def client = new RESTClient("http://localhost:8888/dhtmonitor")
+        def resp = client.post(path : "/dhtmonitor/temperature/10/humidity/10")
+        assert(resp.status==200)
+    }
+
+    // you should be able to get the last N points
+    def "you should be able to get the last N points"() {
+        expect:
+        def client = new RESTClient("http://localhost:8888/dhtmonitor")
+        def resp = client.get(path : "/dhtmonitor/readings/seconds/5")
+        assert(resp.status==200)
+    }
+
+    // you should be able to get all points going back N seconds
+    def "you should be able to get all points going back N seconds"() {
+        expect:
+        def client = new RESTClient("http://localhost:8888/dhtmonitor")
+        def resp = client.get(path : "/dhtmonitor/readings/count/5")
         assert(resp.status==200)
     }
 }
--- src/main/java/com/phg/dhtmonitor/controller/DHTController.java
+++ src/main/java/com/phg/dhtmonitor/controller/DHTController.java
@@ -2,9 +2,9 @@ package com.phg.dhtmonitor.controller;
 
 import com.phg.dhtmonitor.model.*;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 /**
  * Created by milesporter on 2/19/16.
@@ -12,9 +12,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DHTController {
 
-    @RequestMapping("dht")
-    public Dht index() {
-        Dht d = new Dht(10,10);
-        return d;
+    @RequestMapping("hello")
+    public String hello() {
+        return "hello";
     }
+
+    @RequestMapping(value="/temperature/{temperature}/humidity/{humidity}", method=RequestMethod.POST)
+    public String dhtPost( @PathVariable String temperature, @PathVariable String humidity) {
+        Dht dht = new Dht(Float.parseFloat(temperature), Float.parseFloat(humidity));
+        return "OK";
+    }
+
+    @RequestMapping(value="/readings/seconds/{seconds}", method=RequestMethod.GET)
+    public ArrayList<Dht> dhtSeconds(@PathVariable String seconds) {
+        ArrayList dhts = new ArrayList<>();
+        Dht dht = new Dht(0,0);
+        dhts.add(dht);
+        return dhts;
+    }
+
+    @RequestMapping(value="/readings/count/{count}", method=RequestMethod.GET)
+    public ArrayList<Dht> dhtReadings(@PathVariable String count) {
+        ArrayList dhts = new ArrayList<>();
+        Dht dht = new Dht(0,0);
+        dhts.add(dht);
+        return dhts;
+    }
+
 }
