6b07d8b2b8da226188762f36aa26385328cab159
Miles Porter <mporter@paintedharmony.com>
2016-02-26 16:22:37 -0600

    Minor changes.

---
+++ pi/dhtdeploy.sh
@@ -0,0 +1,2 @@
+#This simple file downloads the latest version of the dhtmonitor2.py program to the raspberry pi.
+curl  http://52.87.226.177:8081/artifactory/libs-snapshot-local/prod-jars/pi/dhtmonitor2.py
