9b50f751bbf5d5eda4eb058816acb202bbfc5164
Miles Porter <mporter@paintedharmony.com>
2016-03-07 08:14:47 -0600

    More updates.

--- pi/dhtmonitor2.py
+++ pi/dhtmonitor2.py
@@ -4,7 +4,8 @@
 # This code uses the Beebotte API, you must have an account.
 # You can register here: http://beebotte.com/register
 ############################################################
-
+import sys
+import getopt
 import httplib
 import time
 import Adafruit_DHT
@@ -15,10 +16,11 @@ import time ## Import 'time' library.  Allows us to use 'sleep'
 GPIO.cleanup()
 ### Replace API_KEY and SECRET_KEY with those of your account
 bbt = BBT('a2d79fbe39c3c231a7b2b84ffb105049', '7211c45499bf9cb6819b1d8a9776fc61e49e0fc9ec66ddaf2b6371e4571d49c7')
- webapp = "dhtubuntu.eastus.cloudapp.azure.com:8888"
+webapp = ""
+monitorName = ""
 period = 60 ## Sensor data reporting period (1 minute)
 pin = 4 ## Assuming the DHT11 sensor is connected to GPIO pin number 4
-webapp = "40.76.39.4:8888"
+
 GPIO.setmode(GPIO.BOARD) ## Use BOARD pin numbering
 GPIO.setup(11, GPIO.OUT) ## Setup GPIO pin 11 to OUT
 
@@ -29,6 +31,11 @@ iterations = 445
 speed = 1
 
 def run():
+    # Turn led off.  We'll turn it back on if everything works.
+    GPIO.output(11, False) ## Turn on GPIO pin 7
+    print "Arguments passed: {0:s} ".format(sys.argv[1])
+    webapp = sys.argv[1]
+    monitorName = sys.argv[2]
     ts = time.time()
     st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
     humidity, temperature = Adafruit_DHT.read_retry( Adafruit_DHT.DHT22, pin )
@@ -40,8 +47,9 @@ def run():
           #Send humidity to Beebotte
           humid_resource.write(humidity)
           #Send it all to aws
-          sendWeb(temperature, humidity)
-          Blink(int(iterations),float(speed))
+          sendWeb(webapp, monitorName, temperature, humidity)
+          #  Turn light on to indicate success 
+          GPIO.output(11, True) ## Turn on GPIO pin 7
 
         except Exception as e:
           ## Process exception here
@@ -50,23 +58,15 @@ def run():
     else:
         print "Failed to get reading. Try again!"
 
-## Define function named Blink()
-def Blink(numTimes, speed):
-    for i in range(0,numTimes): ## Run loop numTimes
-        GPIO.output(11, True) ## Turn on GPIO pin 7
-        time.sleep(speed) ## Wait
-        GPIO.output(11, False) ## Switch off GPIO pin 7
-        time.sleep(speed) ## Wait
-    GPIO.cleanup()
-
-def sendWeb(temperature, humidity):
+def sendWeb(webapp, monitorName, temperature, humidity):
    try:
      headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-     conn = httplib.HTTPConnection(webApp)
-     conn.request("POST", "/dhtmonitor/temperature/{0}/humidity/{1}/sensor/musicroom".format(temperature, humidity), "", headers)
+     conn = httplib.HTTPConnection(webapp)
+     conn.request("POST", "/dhtmonitor/temperature/{0}/humidity/{1}/sensor/{2}".format(temperature, humidity, monitorName), "", headers)
      response = conn.getresponse()
      print "dhtmonitor Server Response {0} {1}".format(response.status, response.reason)
    except Exception, e:
       print e
 
 run()
+
