cf6e3fb83cd75178f5805baf83d5b082d3fb6c93
Miles Porter <mporter@paintedharmony.com>
2016-03-07 08:52:25 -0600

    Testing build again...

--- pi/dhtdeploy.sh
+++ pi/dhtdeploy.sh
@@ -1,3 +1,3 @@
 #This simple file downloads the latest version of the dhtmonitor2.py program to the raspberry pi.
-#Note this overwrites the dhtmonitor2.py program on distributed monitors.
+#Note this overwrites the dhtmonitor2.py program on distributed monitors
 curl  http://phgci.eastus.cloudapp.azure.com:808/artifactory/libs-snapshot-local/com/phg/dhtmonitor/pi/dhtmonitor2.py
